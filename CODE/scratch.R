792e06dd4e6f9ec2a11317529c38c906b6eb808d
David Herzberg <dherzberg@wpspublish.com>
2021-02-10 17:01:51 -0800

    Begin documentation of data management best practices

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -1,7 +1,4 @@
-temp1 <- input[as.name("General Live Webinar Experience"):as.name("Usefulness of Content")]
-
-
-input[c(
-  match("General Live Webinar Experience", names(input)):match("Usefulness of Content", names(input))
-)] <- named_super_sub_r_cols
-
+assign(str_c("data", age_range_name, form_name, sep = "_"),
+       suppressMessages(read_csv(url(
+         str_c(urlRemote_path, github_path, input_name)
+       ))))
