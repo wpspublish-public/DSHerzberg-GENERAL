c5693bef21368cd373bfe968ab816f79da2ea212
David Herzberg <dherzberg@wpspublish.com>
2021-02-12 10:23:44 -0800

    test

--- CODE/scratch.R
+++ CODE/scratch.R
@@ -2,3 +2,7 @@ assign(str_c("data", age_range_name, form_name, sep = "_"),
        suppressMessages(read_csv(url(
          str_c(urlRemote_path, github_path, input_name)
        ))))
+assign(str_c("data", age_range_name, form_name, sep = "_"),
+       suppressMessages(read_csv(url(
+         str_c(urlRemote_path, github_path, input_name)
+       ))))
