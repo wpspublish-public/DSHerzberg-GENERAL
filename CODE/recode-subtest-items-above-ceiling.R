93b3371266f7c643415fde69a2e3292c0df0994e
David Herzberg <dherzberg@wpspublish.com>
2021-03-23 16:47:58 -0700

    document ceiling_reached

--- CODE/recode-subtest-items-above-ceiling.R
+++ CODE/recode-subtest-items-above-ceiling.R
@@ -2,25 +2,6 @@ suppressMessages(library(here))
 suppressMessages(library(tidyverse))
 suppressMessages(library(runner))
 
-# special cases that this code needs to handle 
-
-# 1. examiner stops admin before
-# reaching stop rule, so streak_run never reaches 5 - we handle this by not
-# enabling NA_onset until streak run reaches 5. 
-
-# 2. examiner keeps administering after reaching stop rule, so streak_run is 6+.
-# In this case we "correct" the record of an examiner who kept administering
-# after the stop rule was reached. For example, an examiner who administers six
-# fails in a row and then stops, when the stop rule requires stopping after five
-# fails in a row.  Our correction is to change the 0 code in the 6th consecutive fail
-# to NA, which is what it should have been if the examiner correctly applied the
-# stop rule.
-
-
-# 3. examiner admins all items without ever activating stop rule (and streak_run
-# never meets that criterion). In this case handle this by not enabling
-# NA_onset until streak run reaches 5.
-
 urlRemote_path  <- "https://raw.github.com/"
 github_path <- "wpspublish/DSHerzberg-GENERAL/master/INPUT-FILES/"
 fileName_path   <- "recode-above-ceiling-input.csv"
@@ -40,12 +21,11 @@ input_long <- input %>%
     streak_val = case_when(value == 0 ~ streak_run(value, na_rm = F),
                            TRUE ~ NA_integer_),
     ceiling = case_when(
-      # next line will NOT code 1 if ceiling is attained on last item of subtest
       streak_val == 5 & lead(pre) == pre ~ 1,
       TRUE ~ 0)
   )
 
-ceiling <-  input_long %>% 
+ceiling_subtests <-  input_long %>% 
   group_by(ID, pre) %>% 
   summarise(ceiling_count = sum(ceiling)) %>% 
   mutate(ceiling_reached = case_when(
@@ -54,10 +34,8 @@ ceiling <-  input_long %>%
   )) %>% 
   select(-ceiling_count)
 
-# NEXT: evaluate whether simpler predicates could do the job for coding onset and offset_NA
-
 recode_output <- input_long %>% 
-  left_join(ceiling, by = c("ID", "pre")) %>%
+  left_join(ceiling_subtests, by = c("ID", "pre")) %>%
   group_by(ID) %>%
   mutate(
     NA_status = case_when(
