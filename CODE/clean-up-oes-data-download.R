baf9c064cfd567c45f07d186f2bb30ff0b1e41dd
David Herzberg <dherzberg@wpspublish.com>
2021-08-26 16:35:02 -0700

    further refinement of OES cleanup script.

--- CODE/clean-up-oes-data-download.R
+++ CODE/clean-up-oes-data-download.R
@@ -6,8 +6,7 @@ input <- suppressMessages(read_csv(
     "INPUT-FILES/ABAS-3-oes-data-download.csv"
   )
 )) %>% 
-  # select(-FirstName, -LastName, -Location) %>% 
-  select(-FirstName, -LastName) %>% 
+  select(-FirstName, -LastName, -Location) %>%
   rename(
     id = CaseId,
     scale = Title,
@@ -16,19 +15,35 @@ input <- suppressMessages(read_csv(
     item_response_text = ResponseLabel,
     item_response_scored = ResponseValue
     ) %>% 
-  arrange(id, AdministrationFormId) %>% 
-  group_by(id, AdministrationFormId) %>% 
-  summarize()
+  arrange(id, AdministrationFormId)
 
-dup <- input %>% 
-  filter(CaseId == 312274 & TestFormSectionQuestionId == 10488)
+# map TestFormSectionID onto Title: attach labels for ABAS-3 form sections.
+scale_labels_lookup <- bind_cols(unique(input$TestFormSectionId), unique(input$scale)) %>% 
+  set_names(c("TestFormSectionID", "ABAS3_scale"))
 
-write_csv(dup, here("dup.csv"))
-  
-output <- input %>% 
-  select(id, AdministrationFormId, TestFormSectionQuestionId, item_response_scored) %>% 
+write_csv(scale_labels_lookup, here(
+  "OUTPUT-FILES/abas3-scale-labels-lookup.csv"
+))
+
+
+output <- input %>%
+  select(id, TestFormId, scale, item_num, item_response_scored) %>%
   pivot_wider(
-    names_from = TestFormSectionQuestionId,
+    names_from = c(scale, item_num),
+    names_sep = "_",
     values_from = item_response_scored,
   )
+
+
+# %>% 
+#   group_by(id, AdministrationFormId) %>% 
+#   summarize()
+
+dup <- input %>%
+  filter(id == 312274 & TestFormSectionQuestionId == 10488)
+
+write_csv(dup, here("dup.csv"))
+
+
+
 
